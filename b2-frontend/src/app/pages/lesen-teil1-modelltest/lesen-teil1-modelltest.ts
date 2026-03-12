4f04a32d121d1588612c7f4b4f2a25c72c155a5a
Stavros <n.stavros@outlook.de>
2026-03-12 12:21:44 +0100

    Add answer checking button logic

--- b2-frontend/src/app/pages/lesen-teil1-modelltest/lesen-teil1-modelltest.ts
+++ b2-frontend/src/app/pages/lesen-teil1-modelltest/lesen-teil1-modelltest.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { readingTeil1Sets } from '../../data/reading-teil1-sets';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-lesen-teil1-modelltest',
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './lesen-teil1-modelltest.html',
   styleUrl: './lesen-teil1-modelltest.scss',
 })
 export class LesenTeil1Modelltest {
   set = readingTeil1Sets[0];
   answers: { [key: number]: string } = {};
+  results: { [key: number]: boolean } = {}
 
-  selectAnswer(personId: number, articleId: string ) {
+  selectAnswer(personId: number, articleId: string) {
     this.answers[personId] = articleId
     console.log(this.answers);
+  }
+
+  checkAnswers() {
+    this.set.persons.forEach(person => {
+      this.results[person.id] =
+        this.answers[person.id] === person.correctArticle
+    });
+  }
+
+  areAllAnswered() {
+    return Object.keys(this.answers).length === this.set.persons.length;
   }
 }
