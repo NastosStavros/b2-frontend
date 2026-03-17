b584ce66a47c69f420f008f7a72822da26fcde20
Stavros <n.stavros@outlook.de>
2026-03-17 11:49:32 +0100

    Lesen Teil 4 Struktur PC Fertigstellung

--- b2-frontend/src/app/pages/lesen-teil4-modelltest/lesen-teil4-modelltest.ts
+++ b2-frontend/src/app/pages/lesen-teil4-modelltest/lesen-teil4-modelltest.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
 import { readingTeil4Sets } from '../../data/reading-teil4-sets';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-lesen-teil4-modelltest',
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './lesen-teil4-modelltest.html',
   styleUrl: './lesen-teil4-modelltest.scss',
 })
 export class LesenTeil4Modelltest {
   sets = readingTeil4Sets
+  currentSet = readingTeil4Sets[0]
+  currentIndex = 0
+  showResults = false;
+
+
+
+  prevQuestion() {
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.currentSet.questions.length - 1;
+    } else {
+      this.currentIndex--;
+    }
+  }
+
+  nextQuestion() {
+    if (this.currentIndex === this.currentSet.questions.length - 1) {
+      this.currentIndex = 0;
+    } else {
+      this.currentIndex++;
+    }
+  }
+
+  getScore() {
+    let score = 0;
+
+    for (let question of this.currentSet.questions) {
+      if (question.userAnswer === question.correctAnswer) {
+        score++;
+      }
+    }
+
+    return score;
+  }
+
+  getWrongQuestions() {
+    let wrongQuestions = [];
+
+    for (let question of this.currentSet.questions) {
+      if (question.userAnswer !== question.correctAnswer) {
+        wrongQuestions.push(question);
+      }
+    }
+
+    return wrongQuestions;
+  }
+
+  allAnswered() {
+    return this.currentSet.questions.every(q => q.userAnswer);
+  }
 }
